81f575bc0f5de662827f660b215836aada2a7249
Josh Smith <jrs023@uark.edu>
2016-02-25 13:27:50 -0600

    Fixing my mistakes

--- Project 1-10/project1.py
+++
@@ -1,9 +0,0 @@
-#ProjectEuler Number 1
-#by Josh Smith
-
-sum = 0
-for i in range(0,1000):
-    if(i%3 == 0 or i%5 == 0):
-	sum = sum + i
-
-print sum
--- Project_1-10/project9.py
+++
@@ -1,9 +0,0 @@
-#ProjectEuler Number 9
-#by Josh Smith
-
-for x in range(1, 1000):
-	for y in range(x+1, 1000):
-		for z in range (y+1, 1000):
-			if x*x + y*y == z*z and x+y+z == 1000:
-				print str(x) + " " + str(y) + " " + str(z)
-				print x*y*z
